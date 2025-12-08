976f1d2bc23992cd8e549608280b6ad15578bc05
Ahmad <ahmadasgf89@gmail.com>
2025-12-08 07:01:48 +0700

    chore add close button for praktikum 11 GUI kegiatan 1

--- semester-1/praktikum-algopro/praktikum_11_GUI/kegiatan_1.py
+++ semester-1/praktikum-algopro/praktikum_11_GUI/kegiatan_1.py
@@ -11,8 +11,18 @@ root = tk.Tk()
 root.title("Hello GUI")
 root.geometry("300x200")
 
+data_diri = tk.Label(root, text="Data Diri")
+data_diri.pack(pady=1)
+
 for key, value in data.items():
     result_label = tk.Label(root, text=f"{key}: {value}")
     result_label.pack(pady=1)
+
+
+frame_buttons = tk.Frame(root)
+frame_buttons.pack(pady=10)
+
+button_tutup = tk.Button(frame_buttons, text="Close", width=5, font=("Arial", 12), command=root.quit)
+button_tutup.grid(row=0, column=0, padx=5)
 
 root.mainloop()
