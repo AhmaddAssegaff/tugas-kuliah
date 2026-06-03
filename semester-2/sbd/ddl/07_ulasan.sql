45c529e09384392b3fe01bc2c35ca5b0ca680e32
Ahmad <ahmadasgf89@gmail.com>
2026-06-03 15:36:12 +0700

    chore: update ulasan

--- semester-2/sbd/ddl/07_ulasan.sql
+++ semester-2/sbd/ddl/07_ulasan.sql
@@ -1,8 +1,8 @@
 create table ulasan (
     id int auto_increment primary key,
-    id_pesanan int not null,
-    rating int not null,
-    komentar text,
+    id_pesanan INT NOT NULL,
+    rating INT NOT NULL,
+    komentar TEXT NOT NULL,
 
     constraint fk_ulasan_pesanan 
         foreign key (id_pesanan) references pesanan(id) 
